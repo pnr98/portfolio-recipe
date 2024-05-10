a41407e05743d955bde07d4c5a5a98de818fd9e3
pnr98 <pnrl9950@gmail.com>
2024-05-10 15:47:13 +0900

    세팅 및 헤더 틀

---
+++ src/components/common/Header.js
@@ -0,0 +1,82 @@
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: center;
+    height: 60px;
+`
+const NavContainer = styled.nav`
+    display: flex;
+    width: 92%;
+`
+const LogoBox = styled.div`
+    width: 120px;
+    margin-right: 24px;
+    display: flex;
+    align-items: center;
+`
+const LogoImg = styled.img`
+    width: 100%;
+`
+const ContentContainer = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    align-items: center;
+`
+const StyledUl = styled.ul`
+    display: flex;
+    gap: 30px;
+`
+const StyledLi = styled.li`
+    display: flex;
+`
+const StyledLink = styled(Link)`
+    padding: 10px 12px;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+    font-size: 15px;
+    color: var(--brown);
+    font-weight: 700;
+    background: var(--white);
+    &:hover {
+        background: var(--orange-00);
+    }
+    &:active {
+        background: var(--orange-10);
+    }
+`;
+
+export default function Header({isLoggedIn}) {
+    return (
+        <HeaderContainer>
+            <NavContainer>
+                <LogoBox>
+                    <Link to="/home">
+                        <LogoImg src='logo1.png' />
+                    </Link>
+                </LogoBox>
+                <ContentContainer>
+                    <StyledUl>
+                        {!isLoggedIn ? (
+                            <>
+                                <StyledLi><StyledLink to="/login">로그인</StyledLink></StyledLi>
+                                <StyledLi><StyledLink to="/sign-up">회원가입</StyledLink></StyledLi>
+                                <StyledLi><StyledLink to="/guest-login">게스트 로그인</StyledLink></StyledLi>
+                            </>
+                        ) : (
+                            <>
+                                <StyledLi><StyledLink to="/register">레시피 작성</StyledLink></StyledLi>
+                                <StyledLi><StyledLink to="/mypage">마이페이지</StyledLink></StyledLi>
+                                <StyledLi><StyledLink to="/logout">로그아웃</StyledLink></StyledLi>
+                            </>
+                        )}
+                    </StyledUl>
+                </ContentContainer>
+            </NavContainer>
+        </HeaderContainer>
+
+    )
+}
---
+++ src/style/Global.styled.js
@@ -0,0 +1,49 @@
+import { styled, createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --main-width: 1264px;
+        --main-height: calc(100vh - 382px);
+        --orange-00 : #F8F5EA;
+        --orange-10 : #F5C95B;
+        --orange-20 : #F2B824;
+        --orange-30 : #F29F24;
+        --orange-40 : #F27C24;
+        --yellow-10 : #F2D124;
+        --yellow-20 : #F2EB24;
+        --sky-blue : #E1ECF4;
+        --white: #ffffff;
+        --white-10 : #f1f2f3;
+        --black: #000000;
+        --brown: #513000;
+        --gray: #888888;
+        --gray-10 : #D9D9D9;
+        --gray-20 : #525960;
+    }
+    ${reset}
+  * {
+        box-sizing: border-box;
+    }
+
+    body {
+        padding: 0;
+        margin: 0;
+        min-height : 100vh;
+        width: 100vw;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        font: inherit;
+        font-family: 'Noto Sans KR', 'Roboto', sans-serif;
+        vertical-align: baseline;
+        color: var(--black);
+    }
+
+    ol, ul {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+}
+`;
